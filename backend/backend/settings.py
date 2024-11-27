45632d6613461a476fac11a34feba86c306c99aa
HercuLloyd <105641448+HercuLloyd@users.noreply.github.com>
2024-11-26 19:06:12 -0500

    - updated build version

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,7 @@ APPEND_SLASH = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+#
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-vgnb$031d+&k^hz75x5i#xnbg)0*w17u&k*n55u##h6$%5czu5'
